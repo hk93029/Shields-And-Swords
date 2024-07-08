881e349b9ebb97e65f55ba8907fcc810d1e2e7b0
hk93029 <dddio07lamarov51@hotmail.com>
2024-07-08 19:46:24 -0300

    feat: damage digit added

--- enemies/zombie/zombie.gd
+++ enemies/zombie/zombie.gd
@@ -2,10 +2,13 @@ extends CharacterBody2D
 
 var health: int = 5
 var max_health: int = health
+var damage_digit_prefab: PackedScene
+@onready var damage_digit_maker: Marker2D = $DamageDigitMarker
 
 func _ready():
 	$HPBar.value = 100
-	
+	damage_digit_prefab = preload("res://misc/damage_digit/damage_digit.tscn")
+
 
 func set_percent_value_int(value: int) -> void:
 	$HPBar.value = value
@@ -21,6 +24,18 @@ func play_death_effect():
 func damage(damage_count: int) -> void:
 	health = clamp(health-damage_count, 0, health)
 	set_percent_value_int(float(health)/max_health * 100)
+	
+	var damage_digit = damage_digit_prefab.instantiate()
+	damage_digit.value = damage_count
+	
+	
+	if damage_digit_maker != null:
+		damage_digit.global_position = damage_digit_maker.global_position
+	else:
+		damage_digit.global_position = global_position - Vector2(0, 130.218)
+	
+	get_parent().add_child(damage_digit)
+	
 	if health > 0:
 		play_damage_effect()
 	else:
